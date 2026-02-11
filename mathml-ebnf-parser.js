00664ab2a949f29ba2ec33d8736c00d26e47e799
SRCreator <85744292+onu032001@users.noreply.github.com>
2026-02-11 16:42:16 +0900

    수식 요소 표기법 사용 문자열을 처리하는 MathMLEBNFParser 명령함 추가

---
+++ mathml-ebnf-parser.js
@@ -0,0 +1,14 @@
+class MathMLEBNFParser {
+  constructor(mathmlString) {
+    this.mathmlString = mathmlString;
+    this.index = 0;
+  }
+  /**
+   * 수식 요소 표기법 사용 문서의 문자열을 확장형 베커스-누어 형식으로 문자열 처리한 자바스크립트 객체를 보내는 명령함 명령
+   * @returns {Object}
+   */
+  parseMathMLDocument() {
+    // 여기에 들어갈 명령 묶음은 나중에 추가 예정
+  }
+  // 여기에 들어갈 명령함 명령은 나중에 추가 예정
+}
